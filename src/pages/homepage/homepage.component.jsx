73ae16cbb8b4dd21c71738c40f6ad24fab3980ce
Joshi <AG20459@ad.wellpoint.com>
2019-12-02 22:39:21 +0530

    updated menu-item to navigate withRoute HOC

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Directory from '../../components/directory/directory.component';
-import './homepage.styles.scss';
+import Directory from "../../components/directory/directory.component";
+import "./homepage.styles.scss";
 
 export const HomePage = () => (
   <div className="homepage">

45991306a326721da55304338562119aeb93b6d3
Joshi <AG20459@ad.wellpoint.com>
2019-12-08 11:25:02 +0530

    Created card-item component and connected our cart dropdown

---
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { connect } from "react-redux";
+import CartItems from "../cart-item/cart-item.component";
+
+import CustomButton from "../custom-button/custom-button.component";
+import "./cart-dropdown.styles.scss";
+
+const CartDropdown = ({ cartItems }) => (
+  <div className="cart-dropdown">
+    <div className="cart-items">
+      {cartItems.map(cartItem => (
+        <CartItems key={cartItem.id} item={cartItem} />
+      ))}
+    </div>
+    <CustomButton>GO TO CHECKOUT</CustomButton>
+  </div>
+);
+
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems
+});
+
+export default connect(mapStateToProps)(CartDropdown);
---
+++ src/redux/cart/cart.types.js
@@ -0,0 +1,6 @@
+const UserActionTypes = {
+    TOOGLE_CART_HIDDEN: "TOOGLE_CART_HIDDEN",
+    ADD_ITEM: "ADD_ITEM"
+};
+
+export default UserActionTypes;
